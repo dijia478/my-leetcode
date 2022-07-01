41ed4fc45d12fd9256c21c48bc361f1fcab58f60
liuly <liuly@xiaoshouyi.com>
2022-07-01 16:11:39 +0800

    冒泡和选择

---
+++ src/main/java/cn/dijia478/sort/BubbleSort.java
@@ -0,0 +1,49 @@
+package cn.dijia478.sort;
+
+import com.alibaba.fastjson.JSON;
+
+/**
+ * 冒泡排序
+ *
+ * 比较相邻的元素。如果第一个比第二个大，就交换他们两个。
+ * 对每一对相邻元素作同样的工作，从开始第一对到结尾的最后一对。这步做完后，最后的元素会是最大的数。
+ * 针对所有的元素重复以上的步骤，除了最后一个。
+ * 持续每次对越来越少的元素重复上面的步骤，直到没有任何一对数字需要比较。
+ *
+ * 时间复杂度 O(n²)
+ * 额外空间复杂度 O(1)
+ *
+ * @author dijia478
+ * @date 2022/7/1
+ */
+public class BubbleSort {
+
+    public static void bubbleSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = arr.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                }
+            }
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        if (i == j) {
+            return;
+        }
+        arr[i] = arr[i] ^ arr[j];
+        arr[j] = arr[i] ^ arr[j];
+        arr[i] = arr[i] ^ arr[j];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 5, 1, 7, -1, -1, 9, 5, 2, 4, 1};
+        bubbleSort(arr);
+        System.out.println(JSON.toJSONString(arr));
+    }
+
+}
---
+++ src/main/java/cn/dijia478/sort/SelectionSort.java
@@ -0,0 +1,54 @@
+package cn.dijia478.sort;
+
+import com.alibaba.fastjson.JSON;
+
+/**
+ * 选择排序
+ *
+ * 首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置。
+ * 再从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾。
+ * 以此类推，直到所有元素均排序完毕。
+ *
+ * 时间复杂度 O(n²)
+ * 额外空间复杂度 O(1)
+ *
+ * @author dijia478
+ * @date 2022/7/1
+ */
+public class SelectionSort {
+
+    public static void selectionSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+
+        int minIndex;
+        for (int i = 0; i < arr.length - 1; i++) {
+            minIndex = i;
+            // i ~ N-1 上找最小值的下标
+            for (int j = i + 1; j < arr.length; j++) {
+                minIndex = arr[j] < arr[minIndex] ? j : minIndex;
+            }
+            // 交换位置
+            swap(arr, i, minIndex);
+        }
+
+
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        if (i == j) {
+            return;
+        }
+        arr[i] = arr[i] ^ arr[j];
+        arr[j] = arr[i] ^ arr[j];
+        arr[i] = arr[i] ^ arr[j];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 5, 1, 7, -1, -1, 9, 5, 2, 4, 1};
+        selectionSort(arr);
+        System.out.println(JSON.toJSONString(arr));
+    }
+
+}
