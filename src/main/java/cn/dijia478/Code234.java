861c250effc962d2cf6404a64ce931ac3bd9c353
liuly <liuly@xiaoshouyi.com>
2022-07-21 16:10:08 +0800

    修改用例

--- src/main/java/cn/dijia478/Code234.java
+++ src/main/java/cn/dijia478/Code234.java
@@ -23,9 +23,12 @@ public class Code234 {
 
     public static void main(String[] args) {
         ListNode listNode1 = new ListNode(1);
-        ListNode listNode2 = new ListNode(2, listNode1);
-        ListNode listNode3 = new ListNode(2, listNode2);
-        ListNode listNode4 = new ListNode(1, listNode3);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(2);
+        ListNode listNode4 = new ListNode(1);
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
         System.out.println(isPalindrome(listNode4));
 
     }
