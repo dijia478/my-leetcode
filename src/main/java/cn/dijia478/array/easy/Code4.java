650e81bc699a7c59a9bdabf0851e71efb9c2e08d
liuly <liuly@xiaoshouyi.com>
2022-11-21 11:29:47 +0800

    完成code4

---
+++ src/main/java/cn/dijia478/array/easy/Code4.java
@@ -0,0 +1,52 @@
+package cn.dijia478.array.easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 存在重复元素
+ * <p>
+ * 给你一个整数数组 nums 。如果任一值在数组中出现 至少两次 ，返回 true ；如果数组中每个元素互不相同，返回 false 。
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,2,3,1]
+ * 输出：true
+ * 示例 2：
+ * <p>
+ * 输入：nums = [1,2,3,4]
+ * 输出：false
+ * 示例 3：
+ * <p>
+ * 输入：nums = [1,1,1,3,3,4,3,2,4,2]
+ * 输出：true
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 105
+ * -109 <= nums[i] <= 109
+ *
+ * @author dijia478
+ * @date 2022/11/21
+ * @label 数组 哈希表 排序
+ */
+public class Code4 {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1};
+        System.out.println(containsDuplicate(nums));
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
