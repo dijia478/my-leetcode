a998acd3736950c23b9447b25812c48c022413bb
dijia478 <dijia478@163.com>
2022-11-21 17:38:52 +0800

    完成code66

---
+++ src/main/java/cn/dijia478/array/easy/Code66.java
@@ -0,0 +1,65 @@
+package cn.dijia478.array.easy;
+
+import com.alibaba.fastjson.JSON;
+
+/**
+ * 66. 加一
+ * <p>
+ * 给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。
+ * <p>
+ * 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+ * <p>
+ * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：digits = [1,2,3]
+ * 输出：[1,2,4]
+ * 解释：输入数组表示数字 123。
+ * 示例 2：
+ * <p>
+ * 输入：digits = [4,3,2,1]
+ * 输出：[4,3,2,2]
+ * 解释：输入数组表示数字 4321。
+ * 示例 3：
+ * <p>
+ * 输入：digits = [0]
+ * 输出：[1]
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= digits.length <= 100
+ * 0 <= digits[i] <= 9
+ *
+ * @author dijia478
+ * @date 2022/11/21
+ * @label 数组 数学
+ */
+public class Code66 {
+
+    public static void main(String[] args) {
+        int[] digits = {1,9};
+        int[] ints = plusOne(digits);
+        System.out.println(JSON.toJSONString(ints));
+    }
+
+    public static int[] plusOne(int[] digits) {
+        int length = digits.length;
+        for (int i = length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            } else {
+                digits[i] = 0;
+            }
+        }
+
+        // 所有数字都是9，才会有下面的逻辑
+        int[] temp = new int[length + 1];
+        temp[0] = 1;
+        return temp;
+    }
+}
