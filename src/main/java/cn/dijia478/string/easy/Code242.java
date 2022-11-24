67f128e7e0658ecf1ddd6888e1f23c66e5da35a9
dijia478 <dijia478@163.com>
2022-11-24 18:07:44 +0800

    完成code242

---
+++ src/main/java/cn/dijia478/string/easy/Code242.java
@@ -0,0 +1,60 @@
+package cn.dijia478.string.easy;
+
+/**
+ * 242. 有效的字母异位词
+ * <p>
+ * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+ * <p>
+ * 注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
+ * <p>
+ *  
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: s = "anagram", t = "nagaram"
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入: s = "rat", t = "car"
+ * 输出: false
+ *  
+ * <p>
+ * 提示:
+ * <p>
+ * 1 <= s.length, t.length <= 5 * 104
+ * s 和 t 仅包含小写字母
+ *  
+ * <p>
+ * 进阶: 如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？
+ *
+ * @author dijia478
+ * @date 2022/11/24
+ * @label 哈希表 字符串 排序
+ */
+public class Code242 {
+
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagarama";
+        System.out.println(isAnagram(s, t));
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        int[] counts = new int[26];
+        int[] countt = new int[26];
+        char[] chars = s.toCharArray();
+        char[] chart = t.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            counts[chars[i] - 'a']++;
+        }
+        for (int i = 0; i < chart.length; i++) {
+            countt[chart[i] - 'a']++;
+        }
+        for (int i = 0; i < counts.length; i++) {
+            if (counts[i] != countt[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
