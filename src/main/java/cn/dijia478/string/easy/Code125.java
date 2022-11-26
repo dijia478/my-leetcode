43eb2a422654dc6ab86cfd9be723f4c6092c1691
dijia478 <dijia478@163.com>
2022-11-26 10:42:31 +0800

    完成code125

---
+++ src/main/java/cn/dijia478/string/easy/Code125.java
@@ -0,0 +1,76 @@
+package cn.dijia478.string.easy;
+
+import java.time.chrono.IsoChronology;
+
+/**
+ * 125. 验证回文串
+ * <p>
+ * 如果在将所有大写字符转换为小写字符、并移除所有非字母数字字符之后，短语正着读和反着读都一样。则可以认为该短语是一个 回文串 。
+ * <p>
+ * 字母和数字都属于字母数字字符。
+ * <p>
+ * 给你一个字符串 s，如果它是 回文串 ，返回 true ；否则，返回 false 。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入: s = "A man, a plan, a canal: Panama"
+ * 输出：true
+ * 解释："amanaplanacanalpanama" 是回文串。
+ * 示例 2：
+ * <p>
+ * 输入：s = "race a car"
+ * 输出：false
+ * 解释："raceacar" 不是回文串。
+ * 示例 3：
+ * <p>
+ * 输入：s = " "
+ * 输出：true
+ * 解释：在移除非字母数字字符之后，s 是一个空字符串 "" 。
+ * 由于空字符串正着反着读都一样，所以是回文串。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= s.length <= 2 * 105
+ * s 仅由可打印的 ASCII 字符组成
+ *
+ * @author dijia478
+ * @date 2022/11/26
+ * @label 双指针 字符串
+ */
+public class Code125 {
+
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(s));
+    }
+
+    public static boolean isPalindrome(String s) {
+        if (s.length() == 0) {
+            return true;
+        }
+        s = s.toLowerCase();
+        StringBuilder sb = new StringBuilder();
+        char[] chars = s.toCharArray();
+        for (char aChar : chars) {
+            if (aChar >= 'a' && aChar <='z' || aChar >= '0' && aChar <= '9') {
+                sb.append(aChar);
+            }
+        }
+
+        chars = sb.toString().toCharArray();
+        int l = 0;
+        int r = chars.length - 1;
+        while (l < r) {
+            if (chars[l] != chars[r]) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+}
